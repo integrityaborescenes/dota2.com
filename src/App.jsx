544c59a0a5828acf3120abec062a9c8df38252d9
Vladislav Shitov <sayroxskrim@bk.ru>
2025-11-02 18:24:28 +0400

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
     return (
         <>
             <Header/>
+
         </>
     )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,37 @@
-import logo from '../../assets/images/dota2_logo.png'
+import styles from './Header.module.scss'
 
-const Header = (props) => {
-    const {
-
-    } = props
+const Header = () => {
+    const headerNav =['heroes', 'news', 'esports']
 
     return (
-        <>
-            <img src={logo}/>
-            <ul>
-                <li>
-
-                </li>
-            </ul>
-        </>
+        <div className={styles.header}>
+            <div className={styles.headerLeft}>
+                <div className={styles.header__logo}></div>
+                <div className={styles.nav__game}>
+                    <p>Game</p>
+                    <div className={styles.arrowDownGame}></div>
+                </div>
+                <ul className={styles.header__nav}>
+                    {headerNav.map(x =>  <li key={x} className={styles.nav_other}>{x.toUpperCase()}</li>)}
+                </ul>
+            </div>
+            <div className={styles.headerRight}>
+                <div className={styles.languageLogin}>
+                    <p className={styles.login__button}>Login</p>
+                    <div className={styles.language__selector}>
+                        <div className={styles.language__ico}></div>
+                        <p>Select language</p>
+                        <div className={styles.arrowDown}></div>
+                    </div>
+                </div>
+                <button className={styles.play__button}>
+                    <div className={styles.play__buttonCont}>
+                        <div className={styles.steamIco}></div>
+                        <p>Play for free</p>
+                    </div>
+                </button>
+            </div>
+        </div>
     )
 }
 
