fd9689cb1798ee17ef1a40f77a8c7201e2dcc57d
Vladislav Shitov <sayroxskrim@bk.ru>
2025-11-14 00:19:50 +0400

    add activate animation to slider only after user close to heroesSlider block

--- src/components/HeroesSlider/HeroesSlider.jsx
+++ src/components/HeroesSlider/HeroesSlider.jsx
@@ -7,10 +7,11 @@ const HeroesSlider = ({heroImg, heroAttribute, heroName, direction}) => {
     return (
         <div className={`
         ${styles.heroesSlider}
-        ${direction === 'rtl' ? styles.animationLeft : styles.animationRight}
+        ${direction === 'rtl' ? styles.animationLeft :
+            direction === 'ltr' ? styles.animationRight : ''}
         `}>
             <div className={styles.heroContainer}>
-                <img className={styles.heroImg} src={heroImg} loading="lazy"/>
+                <img className={styles.heroImg} src={heroImg}/>
                 <div className={styles.heroInfo}>
                     <img className={styles.heroAttribute} src={heroAttribute}/>
                     <p className={styles.heroName}>{heroName}</p>
--- src/components/WhoWillYouChoose/WhoWillYouChoose.jsx
+++ src/components/WhoWillYouChoose/WhoWillYouChoose.jsx
@@ -3,39 +3,44 @@ import BigBlockWText from "../BigBlockWText/BigBlockWText.jsx";
 import {data} from "./WhoWollYouChooseData.js";
 import HeroesSlider from "../HeroesSlider/HeroesSlider.jsx";
 import {heroes} from '../WhoWillYouChoose/heroesData.js'
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 const WhoWillYouChoose = () => {
-    const isOnBlock = false
+
+    const [isUserOnBlock, setIsUserOnBlock] = useState(false);
     useEffect(() => {
+        const handleScroll = () => {
+            setIsUserOnBlock(window.scrollY > 2300)
+        }
+        window.addEventListener('scroll', handleScroll)
+    }, []);
 
-    }, [isOnBlock]);
 
-    return (
+return (
         <div className={styles.whoWillYouChoose}>
             <div className={styles.whoWillYouChooseContainer}>
                 <BigBlockWText textP={data.textP} hSpanText={data.hSpanText} hText={data.hText} buttonText={data.buttonText}/>
             </div>
             <div className={styles.heroSlider}>
                 <div className={styles.heroRow}>
-                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={'rtl'} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
-                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={'rtl'} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
+                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={isUserOnBlock ? 'rtl' : ''} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
+                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={isUserOnBlock ? 'rtl' : ''} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
                 </div>
                 <div className={styles.heroRow}>
-                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={'ltr'} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
-                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={'ltr'} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
+                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={isUserOnBlock ? 'ltr' : ''} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
+                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={isUserOnBlock ? 'ltr' : ''} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
                 </div>
                 <div className={styles.heroRow}>
-                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={'rtl'} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
-                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={'rtl'} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
+                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={isUserOnBlock ? 'rtl' : ''} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
+                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={isUserOnBlock ? 'rtl' : ''} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
                 </div>
                 <div className={styles.heroRow}>
-                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={'ltr'} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
-                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={'ltr'} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
+                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={isUserOnBlock ? 'ltr' : ''} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
+                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={isUserOnBlock ? 'ltr' : ''} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
                 </div>
                 <div className={styles.heroRow}>
-                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={'rtl'} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
-                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={'rtl'} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
+                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={isUserOnBlock ? 'rtl' : ''} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
+                    {heroes.map((heroInfo) => (<HeroesSlider key={heroInfo.heroName} direction={isUserOnBlock ? 'rtl' : ''} heroAttribute={heroInfo.heroAttribute} heroImg={heroInfo.heroImg} heroName={heroInfo.heroName}/>))}
                 </div>
             </div>
         </div>
