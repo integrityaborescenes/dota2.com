fc63b6a2682fc1b2439cab2b1572e986b4bfe667
Vladislav Shitov <sayroxskrim@bk.ru>
2025-11-16 16:51:57 +0400

    add link to header news and home page View All news

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -2,16 +2,20 @@ import NewsBlock from "./NewsBlock/NewsBlock.jsx";
 import styles from './LatestNews.module.scss'
 import {newsData} from './newsData.js'
 import arrowLeft from '@/assets/images/arrow_left.png'
+import {BASE_URL} from "@/routing/index.js";
+import RouterLink from "@/routing/RouterLink.jsx";
 
 const LatestNews = () => {
     return (
         <div className={styles.latestNewsBlock}>
             <div className={styles.latestNews}>
                 <p>Latest news</p>
-                <div className={styles.viewAll}>
-                    <span>View All</span>
-                    <img className={styles.arrowRight} src={arrowLeft} loading="lazy"/>
-                </div>
+                <RouterLink to={`${BASE_URL}news`} aria-label="News page">
+                    <div className={styles.viewAll}>
+                        <span>View All</span>
+                        <img className={styles.arrowRight} src={arrowLeft} loading="lazy"/>
+                    </div>
+                </RouterLink>
             </div>
             <div className={styles.newsBlocks}>
                 {newsData.filter((x,index) => index < 3).map((x) => (
